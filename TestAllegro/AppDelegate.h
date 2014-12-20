1048f7a5bb73c702db64d902218575506eb3145c
Malcolm <malcolm@Malcolms-Mac-Pro.local>
2014-12-20 20:32:37 +0800

    Initial Commit

---
+++ TestAllegro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestAllegro
+//
+//  Created by Malcolm on 20/12/14.
+//  Copyright (c) 2014 Malcolm Harrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
